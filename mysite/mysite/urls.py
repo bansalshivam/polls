097dca814dcf123173fe2ebaa4dd7d9f13ed2a9c
Shivam Bansal <shivambansal157@gmail.com>
2019-02-15 23:41:37 +0530

    next commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
